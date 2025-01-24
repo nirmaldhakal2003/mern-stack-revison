7c668cf3d6ddd5bd94e2642ce361a8efa1b20b7a
nirmaldhakal2003 <nirmalbsccsit2@gmail.com>
2025-01-24 12:24:36 +0545

    backend completed

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const Blog = require('./model/blogModel')
 const app = express()
 const {multer,storage} = require('./middleware/multerConfig')
 const upload = multer({storage : storage})
+const fs = require('fs')
+const { timeLog } = require('console')
 
 app.use(express.json())
 
@@ -36,7 +38,7 @@ app.post("/blog",upload.single('image') , async (req,res)=>{
         message: "blog api hit successfully!"
     }  )
 })
-
+// for getting all blogs
 app.get("/blog", async(req,res)=>{
     const blogs = await Blog.find()
     res.status(200).json({
@@ -44,6 +46,95 @@ app.get("/blog", async(req,res)=>{
         data : blogs
     })
 })
+
+
+ //for gettting single blog
+app.get("/blog/:id", async(req,res)=>{
+    const id = req.params.id
+    const blog = await Blog.findById(id)
+    if(!blog){
+        return res.status(404).json({
+            message:"No data find"
+            
+        })
+    }
+    res.status(200).json({
+        message:"data featched successfully!",
+        data : blog
+    })
+})
+
+
+//for deleting blogs
+
+app.delete("/blog/:id", async(req,res)=>{
+    const id = req.params.id
+    const blog = await Blog.findById(id)
+    const imageName = blog.image
+    fs.unlink(`storage/${imageName}`,(err)=>{
+        if(err){
+            console.log(err)
+        }
+        else{
+            console.log("file deleted successfully!")
+        }
+    })
+    await Blog.findByIdAndDelete(id)
+    res.status(200).json({
+        message: "blog deleteed successfully!!"
+    })
+})
+
+
+//blog update wala
+app.patch("/blog/:id", upload.single('image'), async (req, res) => {
+    const id = req.params.id;
+    const { title, subtitle, description } = req.body;
+
+    let filename;
+
+    if (req.file) {
+        filename = req.file.filename; 
+        const blog = await Blog.findById(id);
+
+        if (!blog) {
+            return res.status(404).json({
+                message: "Blog not found!"
+            });
+        }
+
+        const oldImageName = blog.image;
+
+        
+        fs.unlink(`storage/${oldImageName}`, (err) => {
+            if (err) {
+                console.error("Error deleting old file:", err);
+            } else {
+                console.log("Old file deleted successfully!");
+            }
+        });
+    }
+
+    await Blog.findByIdAndUpdate(
+        id,
+        {
+            title,
+            subtitle,
+            description,
+            ...(filename && { image: filename }) // Only update image if a new file is uploaded.
+        }
+    );
+
+    res.status(200).json({
+        message: "Blog updated successfully!!"
+    });
+});
+
+
+
+
+
+
 
 
 app.get("/", function(req, res){
